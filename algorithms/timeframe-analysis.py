86ccd04c63aee8fc0ab8bd33938647dcbb55e0a1
Austin <austinnabilblass@gmail.com>
2025-07-30 13:48:12 -0400

    Full Time Analysis

--- algorithms/timeframe-analysis.py
+++ algorithms/timeframe-analysis.py
@@ -1,6 +1,9 @@
 from config import (
-    MIN_AGE_THRESHOLD
+    MIN_AGE_THRESHOLD,
+    MAX_AGE_THRESHOLD
 )
+
+from datetime import utctime as now, timedelta, datetime
 
 def check_market_age(age: int) -> bool:
     """
@@ -13,3 +16,15 @@ def check_market_age(age: int) -> bool:
         bool: True if the market age is above or equal to the minimum threshold, False otherwise.
     """
     return age >= MIN_AGE_THRESHOLD
+
+def check_market_timeline(end_date, age):
+    if not check_market_age(age): return False
+
+    end_date = datetime.fromisoformat(end_date.replace('Z', '+00:00'))
+    
+    time_range = now() + timedelta(days=MAX_AGE_THRESHOLD)
+
+    if end_date <= time_range:
+        return True
+    
+    return False
--- config.py
+++ config.py
@@ -5,3 +5,4 @@ SELL_COUNTDOWN_THRESHOLD = 0.75         # Initiate sell countdown at 75%
 SELL_COUNTDOWN = 7 * 24 * 60 * 60       # One week in seconds
 BUY_COUNTDOWN = 7 * 24 * 60 * 60        # One week in seconds
 MIN_AGE_THRESHOLD = 7 * 24 * 60 * 60    # One week in seconds
+MAX_AGE_THRESHOLD = 60 # Two months
