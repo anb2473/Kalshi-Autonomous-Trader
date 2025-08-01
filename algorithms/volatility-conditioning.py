2b89960c85d204cafd3c962b611d16d018c4b684
Austin <austinnabilblass@gmail.com>
2025-07-31 23:38:30 -0400

    Volatility Conditioning Outline

---
+++ algorithms/volatility-conditioning.py
@@ -0,0 +1,17 @@
+def check_market_volatility():
+    """
+    Analyze prior market volatility to determine if the market is suitable for trading.'
+    """
+    pass
+
+def check_market_category():
+    """
+    Analyze the market category to determine if it is suitable for trading.
+    """
+    pass
+
+def check_trading_volume():
+    """
+    Analyze the trading volume to determine if it is suitable for trading.
+    """
+    pass
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BUY_THRESHOLD = 0.9                     # Immeditately buy at 90%
-BUY_COUNTDOWN_THRESHOLD = 0.8           # Initiate purchase countdown at 80%
-SELL_THRESHOLD = 0.7                    # immediately sell at 70%
+BUY_THRESHOLD = 0.8                     # Immeditately buy at 80%
+BUY_COUNTDOWN_THRESHOLD = 0.7           # Initiate purchase countdown at 70%
+SELL_THRESHOLD = 0.6                    # immediately sell at 60%
 SELL_COUNTDOWN_THRESHOLD = 0.75         # Initiate sell countdown at 75%
 SELL_COUNTDOWN = 7 * 24 * 60 * 60       # One week in seconds
 BUY_COUNTDOWN = 7 * 24 * 60 * 60        # One week in seconds
 MIN_AGE_THRESHOLD = 7 * 24 * 60 * 60    # One week in seconds
-MAX_AGE_THRESHOLD = 60 # Two months
+MAX_AGE_THRESHOLD = 60                  # Two months in days
